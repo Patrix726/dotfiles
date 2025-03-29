daf29c1bca02b3599143fb87efa330fe688b753c
patrix726 <a0911991679@gmail.com>
2025-03-29 13:58:39 +0300

    Added vscode-neovim configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,74 +1,80 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if vim.g.vscode then
+ -- VSCODE Neovim
+  require "vscode_configs/vscode_mappings"
+  require "vscode_configs/vscode_options"
+else
+  -- bootstrap lazy and all plugins
+  local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
-end
+  if not vim.loop.fs_stat(lazypath) then
+    local repo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+  end
 
-vim.opt.rtp:prepend(lazypath)
+  vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = require "configs.lazy"
+  local lazy_config = require "configs.lazy"
 
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
-  },
-
-  { import = "plugins" },
-}, lazy_config)
-
--- require("hardtime").setup()
-
-require('code_runner').setup({
-  filetype = {
-    java = {
-      "cd $dir &&",
-      "javac $fileName &&",
-      "java $fileNameWithoutExt"
+  -- load plugins
+  require("lazy").setup({
+    {
+      "NvChad/NvChad",
+      lazy = false,
+      branch = "v2.5",
+      import = "nvchad.plugins",
+      config = function()
+        require "options"
+      end,
     },
-    python = "python3 -u",
-    typescript = "deno run",
-    rust = {
-      "cd $dir &&",
-      "rustc $fileName &&",
-      "$dir/$fileNameWithoutExt"
-    },
-    c = function(...)
-      c_base = {
+
+    { import = "plugins" },
+  }, lazy_config)
+
+  -- require("hardtime").setup()
+
+  require('code_runner').setup({
+    filetype = {
+      java = {
         "cd $dir &&",
-        "gcc $fileName -o",
-        "/tmp/$fileNameWithoutExt",
-      }
-      local c_exec = {
-        "&& /tmp/$fileNameWithoutExt &&",
-        "rm /tmp/$fileNameWithoutExt",
-      }
-      vim.ui.input({ prompt = "Add more args:" }, function(input)
-        c_base[4] = input
-        vim.print(vim.tbl_extend("force", c_base, c_exec))
-        require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
-      end)
-    end,
-  },
-})
+        "javac $fileName &&",
+        "java $fileNameWithoutExt"
+      },
+      python = "python3 -u",
+      typescript = "deno run",
+      rust = {
+        "cd $dir &&",
+        "rustc $fileName &&",
+        "$dir/$fileNameWithoutExt"
+      },
+      c = function(...)
+        c_base = {
+          "cd $dir &&",
+          "gcc $fileName -o",
+          "/tmp/$fileNameWithoutExt",
+        }
+        local c_exec = {
+          "&& /tmp/$fileNameWithoutExt &&",
+          "rm /tmp/$fileNameWithoutExt",
+        }
+        vim.ui.input({ prompt = "Add more args:" }, function(input)
+          c_base[4] = input
+          vim.print(vim.tbl_extend("force", c_base, c_exec))
+          require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
+        end)
+      end,
+    },
+  })
 
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
+  -- load theme
+  dofile(vim.g.base46_cache .. "defaults")
+  dofile(vim.g.base46_cache .. "statusline")
 
-require "nvchad.autocmds"
+  require "nvchad.autocmds"
 
-vim.schedule(function()
-  require "mappings"
-end)
+  vim.schedule(function()
+    require "mappings"
+  end)
+end
