501e71ef56fb3143cd16f0f9d675852b71794a15
patrix726 <a0911991679@gmail.com>
2025-08-01 16:38:22 +0300

    Migrated from NvChad to custom kickstart neovim config and someother changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,428 +1,109 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
-vim.g.mapleader = " "
+--[[
 
-if vim.g.vscode then
-  -- VSCODE Neovim
-  require "vscode_configs/vscode_mappings"
-  require "vscode_configs/vscode_options"
-else
-  -- bootstrap lazy and all plugins
-  local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+=====================================================================
+==================== READ THIS BEFORE CONTINUING ====================
+=====================================================================
+========                                    .-----.          ========
+========         .----------------------.   | === |          ========
+========         |.-""""""""""""""""""-.|   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||   KICKSTART.NVIM   ||   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||                    ||   |-----|          ========
+========         ||:Tutor              ||   |:::::|          ========
+========         |'-..................-'|   |____o|          ========
+========         `"")----------------(""`   ___________      ========
+========        /::::::::::|  |::::::::::\  \ no mouse \     ========
+========       /:::========|  |==hjkl==:::\  \ required \    ========
+========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
+========                                                     ========
+=====================================================================
+=====================================================================
 
-  if not vim.loop.fs_stat(lazypath) then
-    local repo = "https://github.com/folke/lazy.nvim.git"
-    vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
-  end
+What is Kickstart?
 
-  vim.opt.rtp:prepend(lazypath)
+  Kickstart.nvim is *not* a distribution.
 
-  local lazy_config = require "configs.lazy"
+  Kickstart.nvim is a starting point for your own configuration.
+    The goal is that you can read every line of code, top-to-bottom, understand
+    what your configuration is doing, and modify it to suit your needs.
 
-  -- load plugins
-  require("lazy").setup({
-    {
-      "NvChad/NvChad",
-      lazy = false,
-      branch = "v2.5",
-      import = "nvchad.plugins",
-      config = function()
-        require "options"
-      end,
-    },
+    Once you've done that, you can start exploring, configuring and tinkering to
+    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
+    or immediately breaking it into modular pieces. It's up to you!
 
-    { import = "plugins" },
-  }, lazy_config)
+    If you don't know anything about Lua, I recommend taking some time to read through
+    a guide. One possible example which will only take 10-15 minutes:
+      - https://learnxinyminutes.com/docs/lua/
 
-  -- require("hardtime").setup()
+    After understanding a bit more about Lua, you can use `:help lua-guide` as a
+    reference for how Neovim integrates Lua.
+    - :help lua-guide
+    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
 
-  require("code_runner").setup {
-    filetype = {
-      java = {
-        "cd $dir &&",
-        "javac $fileName &&",
-        "java $fileNameWithoutExt",
-      },
-      python = "python3 -u",
-      typescript = "deno run",
-      rust = {
-        "cd $dir &&",
-        "rustc $fileName &&",
-        "$dir/$fileNameWithoutExt",
-      },
-      c = function(...)
-        c_base = {
-          "cd $dir &&",
-          "gcc $fileName -o",
-          "/tmp/$fileNameWithoutExt",
-        }
-        local c_exec = {
-          "&& /tmp/$fileNameWithoutExt &&",
-          "rm /tmp/$fileNameWithoutExt",
-        }
-        vim.ui.input({ prompt = "Add more args:" }, function(input)
-          c_base[4] = input
-          vim.print(vim.tbl_extend("force", c_base, c_exec))
-          require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
-        end)
-      end,
-    },
-  }
+Kickstart Guide:
 
-  -- load theme
-  dofile(vim.g.base46_cache .. "defaults")
-  dofile(vim.g.base46_cache .. "statusline")
+  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
 
-  require "nvchad.autocmds"
+    If you don't know what this means, type the following:
+      - <escape key>
+      - :
+      - Tutor
+      - <enter key>
 
-  vim.schedule(function()
-    require "mappings"
-  end)
+    (If you already know the Neovim basics, you can skip this step.)
 
-  local neogit = require "neogit"
+  Once you've completed that, you can continue working through **AND READING** the rest
+  of the kickstart init.lua.
 
-  neogit.setup {
-    -- Hides the hints at the top of the status buffer
-    disable_hint = false,
-    -- Disables changing the buffer highlights based on where the cursor is.
-    disable_context_highlighting = false,
-    -- Disables signs for sections/items/hunks
-    disable_signs = false,
-    -- Offer to force push when branches diverge
-    prompt_force_push = true,
-    -- Changes what mode the Commit Editor starts in. `true` will leave nvim in normal mode, `false` will change nvim to
-    -- insert mode, and `"auto"` will change nvim to insert mode IF the commit message is empty, otherwise leaving it in
-    -- normal mode.
-    disable_insert_on_commit = "auto",
-    -- When enabled, will watch the `.git/` directory for changes and refresh the status buffer in response to filesystem
-    -- events.
-    filewatcher = {
-      interval = 1000,
-      enabled = true,
-    },
-    -- "ascii"   is the graph the git CLI generates
-    -- "unicode" is the graph like https://github.com/rbong/vim-flog
-    -- "kitty"   is the graph like https://github.com/isakbm/gitgraph.nvim - use https://github.com/rbong/flog-symbols if you don't use Kitty
-    graph_style = "ascii",
-    -- Show relative date by default. When set, use `strftime` to display dates
-    commit_date_format = nil,
-    log_date_format = nil,
-    -- Show message with spinning animation when a git command is running.
-    process_spinner = false,
-    -- Used to generate URL's for branch popup action "pull request".
-    git_services = {
-      ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
-      ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
-      ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
-      ["azure.com"] = "https://dev.azure.com/${owner}/_git/${repository}/pullrequestcreate?sourceRef=${branch_name}&targetRef=${target}",
-    },
-    -- Allows a different telescope sorter. Defaults to 'fuzzy_with_index_bias'. The example below will use the native fzf
-    -- sorter instead. By default, this function returns `nil`.
-    telescope_sorter = function()
-      return require("telescope").extensions.fzf.native_fzf_sorter()
-    end,
-    -- Persist the values of switches/options within and across sessions
-    remember_settings = true,
-    -- Scope persisted settings on a per-project basis
-    use_per_project_settings = true,
-    -- Table of settings to never persist. Uses format "Filetype--cli-value"
-    ignored_settings = {
-      "NeogitPushPopup--force-with-lease",
-      "NeogitPushPopup--force",
-      "NeogitPullPopup--rebase",
-      "NeogitCommitPopup--allow-empty",
-      "NeogitRevertPopup--no-edit",
-    },
-    -- Configure highlight group features
-    highlight = {
-      italic = true,
-      bold = true,
-      underline = true,
-    },
-    -- Set to false if you want to be responsible for creating _ALL_ keymappings
-    use_default_keymaps = true,
-    -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
-    -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
-    auto_refresh = true,
-    -- Value used for `--sort` option for `git branch` command
-    -- By default, branches will be sorted by commit date descending
-    -- Flag description: https://git-scm.com/docs/git-branch#Documentation/git-branch.txt---sortltkeygt
-    -- Sorting keys: https://git-scm.com/docs/git-for-each-ref#_options
-    sort_branches = "-committerdate",
-    -- Default for new branch name prompts
-    initial_branch_name = "",
-    -- Change the default way of opening neogit
-    kind = "tab",
-    -- Disable line numbers
-    disable_line_numbers = true,
-    -- Disable relative line numbers
-    disable_relative_line_numbers = true,
-    -- The time after which an output console is shown for slow running commands
-    console_timeout = 2000,
-    -- Automatically show console if a command takes more than console_timeout milliseconds
-    auto_show_console = true,
-    -- Automatically close the console if the process exits with a 0 (success) status
-    auto_close_console = true,
-    notification_icon = "󰊢",
-    status = {
-      show_head_commit_hash = true,
-      recent_commit_count = 10,
-      HEAD_padding = 10,
-      HEAD_folded = false,
-      mode_padding = 3,
-      mode_text = {
-        M = "modified",
-        N = "new file",
-        A = "added",
-        D = "deleted",
-        C = "copied",
-        U = "updated",
-        R = "renamed",
-        DD = "unmerged",
-        AU = "unmerged",
-        UD = "unmerged",
-        UA = "unmerged",
-        DU = "unmerged",
-        AA = "unmerged",
-        UU = "unmerged",
-        ["?"] = "",
-      },
-    },
-    commit_editor = {
-      kind = "tab",
-      show_staged_diff = true,
-      -- Accepted values:
-      -- "split" to show the staged diff below the commit editor
-      -- "vsplit" to show it to the right
-      -- "split_above" Like :top split
-      -- "vsplit_left" like :vsplit, but open to the left
-      -- "auto" "vsplit" if window would have 80 cols, otherwise "split"
-      staged_diff_split_kind = "split",
-      spell_check = true,
-    },
-    commit_select_view = {
-      kind = "tab",
-    },
-    commit_view = {
-      kind = "vsplit",
-      verify_commit = vim.fn.executable "gpg" == 1, -- Can be set to true or false, otherwise we try to find the binary
-    },
-    log_view = {
-      kind = "tab",
-    },
-    rebase_editor = {
-      kind = "auto",
-    },
-    reflog_view = {
-      kind = "tab",
-    },
-    merge_editor = {
-      kind = "auto",
-    },
-    description_editor = {
-      kind = "auto",
-    },
-    tag_editor = {
-      kind = "auto",
-    },
-    preview_buffer = {
-      kind = "floating_console",
-    },
-    popup = {
-      kind = "split",
-    },
-    stash = {
-      kind = "tab",
-    },
-    refs_view = {
-      kind = "tab",
-    },
-    signs = {
-      -- { CLOSED, OPENED }
-      hunk = { "", "" },
-      item = { ">", "v" },
-      section = { ">", "v" },
-    },
-    -- Each Integration is auto-detected through plugin presence, however, it can be disabled by setting to `false`
-    integrations = {
-      -- If enabled, use telescope for menu selection rather than vim.ui.select.
-      -- Allows multi-select and some things that vim.ui.select doesn't.
-      telescope = nil,
-      -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
-      -- The diffview integration enables the diff popup.
-      --
-      -- Requires you to have `sindrets/diffview.nvim` installed.
-      diffview = nil,
+  Next, run AND READ `:help`.
+    This will open up a help window with some basic information
+    about reading, navigating and searching the builtin help documentation.
 
-      -- If enabled, uses fzf-lua for menu selection. If the telescope integration
-      -- is also selected then telescope is used instead
-      -- Requires you to have `ibhagwan/fzf-lua` installed.
-      fzf_lua = nil,
+    This should be the first place you go to look when you're stuck or confused
+    with something. It's one of my favorite Neovim features.
 
-      -- If enabled, uses mini.pick for menu selection. If the telescope integration
-      -- is also selected then telescope is used instead
-      -- Requires you to have `echasnovski/mini.pick` installed.
-      mini_pick = nil,
-    },
-    sections = {
-      -- Reverting/Cherry Picking
-      sequencer = {
-        folded = false,
-        hidden = false,
-      },
-      untracked = {
-        folded = false,
-        hidden = false,
-      },
-      unstaged = {
-        folded = false,
-        hidden = false,
-      },
-      staged = {
-        folded = false,
-        hidden = false,
-      },
-      stashes = {
-        folded = true,
-        hidden = false,
-      },
-      unpulled_upstream = {
-        folded = true,
-        hidden = false,
-      },
-      unmerged_upstream = {
-        folded = false,
-        hidden = false,
-      },
-      unpulled_pushRemote = {
-        folded = true,
-        hidden = false,
-      },
-      unmerged_pushRemote = {
-        folded = false,
-        hidden = false,
-      },
-      recent = {
-        folded = true,
-        hidden = false,
-      },
-      rebase = {
-        folded = true,
-        hidden = false,
-      },
-    },
-    mappings = {
-      commit_editor = {
-        ["q"] = "Close",
-        ["<c-c><c-c>"] = "Submit",
-        ["<c-c><c-k>"] = "Abort",
-        ["<m-p>"] = "PrevMessage",
-        ["<m-n>"] = "NextMessage",
-        ["<m-r>"] = "ResetMessage",
-      },
-      commit_editor_I = {
-        ["<c-c><c-c>"] = "Submit",
-        ["<c-c><c-k>"] = "Abort",
-      },
-      rebase_editor = {
-        ["p"] = "Pick",
-        ["r"] = "Reword",
-        ["e"] = "Edit",
-        ["s"] = "Squash",
-        ["f"] = "Fixup",
-        ["x"] = "Execute",
-        ["d"] = "Drop",
-        ["b"] = "Break",
-        ["q"] = "Close",
-        ["<cr>"] = "OpenCommit",
-        ["gk"] = "MoveUp",
-        ["gj"] = "MoveDown",
-        ["<c-c><c-c>"] = "Submit",
-        ["<c-c><c-k>"] = "Abort",
-        ["[c"] = "OpenOrScrollUp",
-        ["]c"] = "OpenOrScrollDown",
-      },
-      rebase_editor_I = {
-        ["<c-c><c-c>"] = "Submit",
-        ["<c-c><c-k>"] = "Abort",
-      },
-      finder = {
-        ["<cr>"] = "Select",
-        ["<c-c>"] = "Close",
-        ["<esc>"] = "Close",
-        ["<c-n>"] = "Next",
-        ["<c-p>"] = "Previous",
-        ["<down>"] = "Next",
-        ["<up>"] = "Previous",
-        ["<tab>"] = "InsertCompletion",
-        ["<space>"] = "MultiselectToggleNext",
-        ["<s-space>"] = "MultiselectTogglePrevious",
-        ["<c-j>"] = "NOP",
-        ["<ScrollWheelDown>"] = "ScrollWheelDown",
-        ["<ScrollWheelUp>"] = "ScrollWheelUp",
-        ["<ScrollWheelLeft>"] = "NOP",
-        ["<ScrollWheelRight>"] = "NOP",
-        ["<LeftMouse>"] = "MouseClick",
-        ["<2-LeftMouse>"] = "NOP",
-      },
-      -- Setting any of these to `false` will disable the mapping.
-      popup = {
-        ["?"] = "HelpPopup",
-        ["A"] = "CherryPickPopup",
-        ["d"] = "DiffPopup",
-        ["M"] = "RemotePopup",
-        ["P"] = "PushPopup",
-        ["X"] = "ResetPopup",
-        ["Z"] = "StashPopup",
-        ["i"] = "IgnorePopup",
-        ["t"] = "TagPopup",
-        ["b"] = "BranchPopup",
-        ["B"] = "BisectPopup",
-        ["w"] = "WorktreePopup",
-        ["c"] = "CommitPopup",
-        ["f"] = "FetchPopup",
-        ["l"] = "LogPopup",
-        ["m"] = "MergePopup",
-        ["p"] = "PullPopup",
-        ["r"] = "RebasePopup",
-        ["v"] = "RevertPopup",
-      },
-      status = {
-        ["j"] = "MoveDown",
-        ["k"] = "MoveUp",
-        ["o"] = "OpenTree",
-        ["q"] = "Close",
-        ["I"] = "InitRepo",
-        ["1"] = "Depth1",
-        ["2"] = "Depth2",
-        ["3"] = "Depth3",
-        ["4"] = "Depth4",
-        ["Q"] = "Command",
-        ["<tab>"] = "Toggle",
-        ["x"] = "Discard",
-        ["s"] = "Stage",
-        ["S"] = "StageUnstaged",
-        ["<c-s>"] = "StageAll",
-        ["u"] = "Unstage",
-        ["K"] = "Untrack",
-        ["U"] = "UnstageStaged",
-        ["y"] = "ShowRefs",
-        ["$"] = "CommandHistory",
-        ["Y"] = "YankSelected",
-        ["<c-r>"] = "RefreshBuffer",
-        ["<cr>"] = "GoToFile",
-        ["<s-cr>"] = "PeekFile",
-        ["<c-v>"] = "VSplitOpen",
-        ["<c-x>"] = "SplitOpen",
-        ["<c-t>"] = "TabOpen",
-        ["{"] = "GoToPreviousHunkHeader",
-        ["}"] = "GoToNextHunkHeader",
-        ["[c"] = "OpenOrScrollUp",
-        ["]c"] = "OpenOrScrollDown",
-        ["<c-k>"] = "PeekUp",
-        ["<c-j>"] = "PeekDown",
-        ["<c-n>"] = "NextSection",
-        ["<c-p>"] = "PreviousSection",
-      },
-    },
-  }
-end
+    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
+    which is very useful when you're not exactly sure of what you're looking for.
+
+  I have left several `:help X` comments throughout the init.lua
+    These are hints about where to find more information about the relevant settings,
+    plugins or Neovim features used in Kickstart.
+
+   NOTE: Look for lines like this
+
+    Throughout the file. These are for you, the reader, to help you understand what is happening.
+    Feel free to delete them once you know what you're doing, but they should serve as a guide
+    for when you are first encountering a few different constructs in your Neovim config.
+
+If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
+
+I hope you enjoy your Neovim journey,
+- TJ
+
+P.S. You can delete this when you're done too. It's your config now! :)
+--]]
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- [[ Setting options ]]
+require 'options'
+
+-- [[ Basic Keymaps ]]
+require 'keymaps'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'lazy-bootstrap'
+
+-- [[ Configure and install plugins ]]
+require 'lazy-plugins'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
