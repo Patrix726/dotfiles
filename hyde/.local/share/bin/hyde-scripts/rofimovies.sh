3f7a27321448508bd919de342e4cd5041248e78d
patrix726 <a0911991679@gmail.com>
2025-08-05 19:17:49 +0300

    Some additional scripts and config changes

---
+++ hyde/.local/share/bin/hyde-scripts/rofimovies.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+confDir="$HOME/.config"
+roConf="$confDir/rofi/movies.rasi"
+
+BASE_DIR="/mnt/668E28458E280FDB/Movies"  # Main movies directory
+DOW_DIR="/mnt/668E28458E280FDB/Downloads"  # Download movies directory
+
+# List all folders relative to BASE_DIR
+folder_list=$(find "$BASE_DIR" "$DOW_DIR"\
+                ! -path "$BASE_DIR" \
+                ! -path "$DOW_DIR" \
+                ! -name "Other" \
+                ! -name "Subs" \
+                -type d | sed -e "s|$DOW_DIR/||" -e "s|$BASE_DIR/||")
+
+# Add this above if you want to disclude video
+# ! -path "$DOW_DIR/Videos*" \
+
+# Show in rofi with fuzzy search
+selected=$(echo "$folder_list" | rofi -dmenu -i -p "Select movie folder" -config "$roConf")
+
+# Handle selection
+if [[ -n "$selected" ]]; then
+	if [[ -d "$BASE_DIR/$selected" ]] ; then
+        nautilus "$BASE_DIR/$selected"
+    elif [[ -d "$DOW_DIR/$selected" ]] ; then
+        nautilus "$DOW_DIR/$selected"
+    fi
+fi
---
+++ hyde/.local/share/bin/hyde-scripts/rofinotes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+BIN_DIR="$HOME/.local/bin"
+
+$BIN_DIR/rofi_notes.sh -a "Nebil Rah" -e "kitty nvim" -o "$HOME/Documents/obsidian-notes/1 - Rough Notes/Rofi Notes"
